54d73df69b37cbf910434f0c3fad7229c1387cff
Ernest <zyperis14@gmail.com>
2025-08-04 20:05:09 +0300

    multiply dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import { HomePage } from './pages/public/Home';
 import { PublicLayout } from './templates/PublicLayout';
 
