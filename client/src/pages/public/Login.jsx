5e73f32e56670e73197ceff97e55b1e78a8be3e8
Ernest <zyperis14@gmail.com>
2025-08-07 16:47:07 +0300

    isLoggedOut

--- client/src/pages/public/Login.jsx
+++ client/src/pages/public/Login.jsx
@@ -1,13 +1,29 @@
+import { Link } from "react-router";
 import { LoginForm } from "../../components/forms/LoginForm";
 import { PublicPageTitle } from "../../components/PublicPageTitle";
 
 export function LoginPage() {
+    const isLoggedIn = true;
+
     return (
         <main className="min-page-height">
             <PublicPageTitle title={'Login'} />
+
             <div className="container">
                 <div className="row">
-                    <LoginForm />
+                    {
+                        isLoggedIn
+                        ?
+                        <>
+                            <p>Einamuoju metu prie sistemos prisijunge vartotojai negali prisijungti prie kitos paskyros. Noredami ta atlikti - atsijunkite nuo dartines paskyros</p>
+                            <div className="d-flex gap-3">
+                                <Link to='/' className="btn btn-primary">Go home</Link>
+                                or
+                                <Link to='/logout' className="btn btn-primary">Logout</Link>
+                            </div>
+                        </>
+                        : <LoginForm />
+                    }
                 </div>
             </div>
         </main>
--- client/src/pages/public/Register.jsx
+++ client/src/pages/public/Register.jsx
@@ -1,13 +1,28 @@
+import { Link } from "react-router";
 import { RegisterForm } from "../../components/forms/RegisterForm";
 import { PublicPageTitle } from "../../components/PublicPageTitle";
 
 export function RegisterPage() {
+    const isLoggedIn = true;
+
     return (
         <main className="min-page-height">
             <PublicPageTitle title={'Register'}/>
             <div className="container">
                 <div className="row">
-                    <RegisterForm />
+                                        {
+                        isLoggedIn
+                        ?
+                        <>
+                            <p>Einamuoju metu prie sistemos prisijunge vartotojai negali registruoti kitos paskyros. Noredami ta atlikti - atsijunkite nuo dartines paskyros</p>
+                            <div className="d-flex gap-3">
+                                <Link to='/' className="btn btn-primary">Go home</Link>
+                                or
+                                <Link to='/logout' className="btn btn-primary">Logout</Link>
+                            </div>
+                        </>
+                        : <RegisterForm />
+                    }
                 </div>
             </div>
 
