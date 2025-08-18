57d1e33d9e8af75165de247cf2236b9ce7697049
Ernest <zyperis14@gmail.com>
2025-08-18 08:37:19 +0300

    up

--- client/src/pages/admin/categories/NewCategory.jsx
+++ client/src/pages/admin/categories/NewCategory.jsx
@@ -2,13 +2,15 @@ import { AdminPageTitle } from "../../../components/AdminPageTitle";
 import { AdminCategoryForm } from "../../../components/forms/AdminCategoryForm";
 
 export function AdminNewCategoryPage() {
-    return ( 
+    return (
         <main>
-            <AdminPageTitle title={'New category'} />
+            <AdminPageTitle title="New category" />
 
             <div className="container">
                 <div className="row">
-                    <AdminCategoryForm />
+                    <AdminCategoryForm
+                        api="http://localhost:5519/api/admin/categories"
+                        method="POST" />
                 </div>
             </div>
         </main>
--- client/src/pages/admin/categories/ViewCategory.jsx
+++ client/src/pages/admin/categories/ViewCategory.jsx
@@ -1,59 +1,27 @@
-import { useParams } from "react-router";
+import { useParams } from 'react-router';
 import { AdminPageTitle } from "../../../components/AdminPageTitle";
 import { Alert } from "../../../components/Alert";
-import { useContext } from "react";
-import { CategoriesContext } from "../../../context/categories/CategoriesContext";
+import { useContext } from 'react';
+import { CategoriesContext } from '../../../context/categories/CategoriesContext';
+import { AdminViewCategoryTable } from '../../../components/AdminViewCategoryTable';
 
 export function AdminViewCategoryPage() {
+    const { getAdminCategoryByUrlSlug } = useContext(CategoriesContext);
     const { category } = useParams();
-    const {getAdminCategoryByUrlSlug} = useContext(CategoriesContext);
 
     const categoryData = getAdminCategoryByUrlSlug(category);
 
-    if (!categoryData) {
-        return (
-            <main>
-                <AdminPageTitle title={`View category: "${category}" `} />
-                <div className="col-12 col-md-9 mt-5">
-                    <Alert text='Norima kategorija nerasta, todel jos perziureti yra neimanomas.' />
-                </div>
-            </main>
-        )
-    }
-
     return (
         <main>
-            <AdminPageTitle title={`View category: "${category}" `} />
+            <AdminPageTitle title={`View category: "${category}"`} />
 
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-9 mt-5">
-                        <table className="table table-bordered border-primary">
-                            <tbody>
-                                <tr className="mb-3">
-                                    <td>Title</td>
-                                    <td>{categoryData.title}</td>
-                                </tr>
-                                <tr className="mb-3">
-                                    <td>Url slug</td>
-                                    <td>{categoryData.url}</td>
-                                </tr>
-                                <tr className="mb-3">
-                                    <td>Description</td>
-                                    <td>{categoryData.description}</td>
-                                </tr>
-                                <tr className="mb-3">
-                                    <td>Status</td>
-                                    <td>
-                                        {
-                                        categoryData.status === 'published'
-                                        ? <span className="badge text-bg-success">Published</span>
-                                        : <span className="badge text-bg-warning">Draft</span>
-                                        }
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        {categoryData
+                            ? <AdminViewCategoryTable data={categoryData} />
+                            : <Alert text='Norima kategorija nerasta, todel jos perziureti yra neimanomas.' />
+                        }
                     </div>
                 </div>
             </div>
